4a52973c5c3eec68cb83d91b28e4bf988ad8e7d5
Rifqi Ryandi <40005-c02hg3huq05d@40005-C02HG3HUQ05Ds-MacBook-Pro.local>
2023-12-08 00:11:15 +0700

    change size

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
 module.exports = {
     configureWebpack: {
         performance: {
-            maxAssetSize: 1048576, // int (in bytes),
+            maxAssetSize: 114857600, // int (in bytes),
             maxEntrypointSize: 114857600, // int (in bytes)
         },
-        optimization: {
-            splitChunks: {
-                minSize: 10000,
-                maxSize: 250000,
-            },
-
-        }
     }
 }
